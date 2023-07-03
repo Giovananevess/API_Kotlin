8f5d961edac99d1d3a792b5470c0bcfe7b4d2def
Giovananevess <giovana_neves2001@outlook.com>
2023-07-03 17:48:13 -0300

    commit inicial

---
+++ app/src/main/java/com/example/kotlintrevo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.kotlintrevo
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
